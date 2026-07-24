2e5855ce9023e50cd52c1365013a185f0c8b4790
jhay arranguez <jhaymiee21@gmail.com>
2026-07-24 14:26:09 +0800

    picker for selection text

--- helpers/picker.js
+++ helpers/picker.js
@@ -9,7 +9,7 @@ export class Picker {
         const target = Number[targetNumber];
 
         let attempts = 0;
-        let lastItem = "";
+        let lastItem = null;
 
         while(attempts < maxScroll) { 
             const inputPick = await picker.$(this.inputElement);
@@ -39,5 +39,52 @@ export class Picker {
             attempts++;
         }
         throw new Error(`number: ${target} not found after ${maxScroll} of scrolls`);
+    }
+
+
+    async selectByText(selector, targetText, maxScroll = 30) { 
+        const picker = await this.driver.$(selector);
+        const target = String[targetText];
+
+        let attempts = 0;
+        let currentDirection = 'down';
+        let lastItem = "";
+        let isFlipped = false;
+
+        while(attempts < maxScroll) { 
+            const inputPick = await picker.$(this.inputElement);
+            const value = (await inputPick.getText() | await inputPick.getAttribute("text") | "").trim();
+
+            if(value === target) { 
+                return;
+            }
+
+            if(value === lastItem) { 
+                if(!isFlipped) { 
+                    console.log(`reach last item and still unable to find ${value}, scrolling up...`);
+
+                    isFlipped = true;
+                    currentDirection = 'up';
+                    lastItem = value;
+                    attempts = 0;
+                    continue;
+                } else {
+                    throw new Error(`text: ${value} now found or does not exist in the list`);
+                }
+            }
+
+            lastItem = value;
+
+            await this.driver.execute('mobile:scrollGesture', {
+                elementId: picker.elementId,
+                direction: currentDirection, 
+                percent: 0.5, 
+                speed: 800,
+            });
+
+            await this.driver.pause(300);
+            attempts++;
+        }
+        throw new Error(`text: ${value} not found after ${maxScroll} of scrolls`);
     }
 }
