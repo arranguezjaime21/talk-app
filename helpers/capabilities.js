0ca741bfd1eb2486a5f58cdf7c09df6eee81d54a
jhay arranguez <jhaymiee21@gmail.com>
2026-07-24 16:27:53 +0800

    setup

---
+++ helpers/capabilities.js
@@ -0,0 +1,13 @@
+export const emulatorCaps = {
+    "platformName": "Android",
+    "appium:deviceName": "emulator-5554",
+    "appium:appPackage": "com.fdc_machetalk_broadcaster",
+    "appium:appActivity": "com.fdc_machetalk_broadcaster.Activity.RootActivity",
+    "appium:automationName": "UiAutomator2",
+    "appium:noReset": true
+}
+
+export const emulatorCapsReset = {
+    ...emulatorCaps, 
+    "appium:noReset": false
+}
